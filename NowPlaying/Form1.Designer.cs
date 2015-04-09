fbc657f3965dba89e966cfa0676851ade617064d
Luch <yupuzu@gmail.com>
2015-04-09 16:34:01 +0300

    stuff

---
+++ NowPlaying/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace NowPlaying
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.txtDebug = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtArtist = new System.Windows.Forms.TextBox();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Artist:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Saving to:";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(12, 143);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(559, 20);
+            this.txtPath.TabIndex = 4;
+            // 
+            // txtDebug
+            // 
+            this.txtDebug.Location = new System.Drawing.Point(11, 204);
+            this.txtDebug.Multiline = true;
+            this.txtDebug.Name = "txtDebug";
+            this.txtDebug.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtDebug.Size = new System.Drawing.Size(556, 195);
+            this.txtDebug.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Title:";
+            // 
+            // txtArtist
+            // 
+            this.txtArtist.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtArtist.Location = new System.Drawing.Point(18, 34);
+            this.txtArtist.Name = "txtArtist";
+            this.txtArtist.ReadOnly = true;
+            this.txtArtist.Size = new System.Drawing.Size(550, 29);
+            this.txtArtist.TabIndex = 7;
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtTitle.Location = new System.Drawing.Point(18, 82);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.ReadOnly = true;
+            this.txtTitle.Size = new System.Drawing.Size(550, 29);
+            this.txtTitle.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 185);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Debug:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 418);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.txtArtist);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDebug);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Osu! NowPlaying";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.TextBox txtDebug;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtArtist;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ NowPlaying/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NowPlaying
+{
+    public partial class Form1 : Form
+    {
+        private CustomWindow cw;
+        private Action<string> update;
+        public Form1()
+        {
+            InitializeComponent();
+            update = WriteFile;
+            cw = new CustomWindow("MsnMsgrUIManager", update);
+            txtPath.Text = Application.StartupPath + @"\now_playing.txt";
+        }
+
+        private void WriteFile(string s)
+        {
+            string[] separators = new string[] { @"\0" };
+            string[] result = s.Split(separators, StringSplitOptions.None);
+            txtArtist.Text = result[5];
+            txtTitle.Text = result[4];
+            System.IO.File.WriteAllText(Application.StartupPath + @"\now_playing.txt", result[5] + " - " + result[4]);
+
+            foreach (string x in result)
+            {
+                txtDebug.Text += x + "\r\n";
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            cw.Dispose();
+        }
+    }
+
+    class CustomWindow : IDisposable
+    {
+        delegate IntPtr WndProc(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
+
+        [System.Runtime.InteropServices.StructLayout(
+            System.Runtime.InteropServices.LayoutKind.Sequential,
+           CharSet = System.Runtime.InteropServices.CharSet.Unicode
+        )]
+        struct WNDCLASS
+        {
+            public uint style;
+            public IntPtr lpfnWndProc;
+            public int cbClsExtra;
+            public int cbWndExtra;
+            public IntPtr hInstance;
+            public IntPtr hIcon;
+            public IntPtr hCursor;
+            public IntPtr hbrBackground;
+            [System.Runtime.InteropServices.MarshalAs(System.Runtime.InteropServices.UnmanagedType.LPWStr)]
+            public string lpszMenuName;
+            [System.Runtime.InteropServices.MarshalAs(System.Runtime.InteropServices.UnmanagedType.LPWStr)]
+            public string lpszClassName;
+        }
+
+        [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
+        static extern System.UInt16 RegisterClassW(
+            [System.Runtime.InteropServices.In] ref WNDCLASS lpWndClass
+        );
+
+        [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr CreateWindowExW(
+           UInt32 dwExStyle,
+           [System.Runtime.InteropServices.MarshalAs(System.Runtime.InteropServices.UnmanagedType.LPWStr)]
+       string lpClassName,
+           [System.Runtime.InteropServices.MarshalAs(System.Runtime.InteropServices.UnmanagedType.LPWStr)]
+       string lpWindowName,
+           UInt32 dwStyle,
+           Int32 x,
+           Int32 y,
+           Int32 nWidth,
+           Int32 nHeight,
+           IntPtr hWndParent,
+           IntPtr hMenu,
+           IntPtr hInstance,
+           IntPtr lpParam
+        );
+
+        [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
+        static extern System.IntPtr DefWindowProcW(
+            IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam
+        );
+
+        [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
+        static extern bool DestroyWindow(
+            IntPtr hWnd
+        );
+
+        private const int ERROR_CLASS_ALREADY_EXISTS = 1410;
+
+        private bool m_disposed;
+        private IntPtr m_hwnd;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!m_disposed)
+            {
+                if (disposing)
+                {
+                    // Dispose managed resources
+                }
+
+                // Dispose unmanaged resources
+                if (m_hwnd != IntPtr.Zero)
+                {
+                    DestroyWindow(m_hwnd);
+                    m_hwnd = IntPtr.Zero;
+                }
+            }
+        }
+
+        public CustomWindow(string class_name, Action<string> u)
+        {
+            update = u;
+            if (class_name == null) throw new System.Exception("class_name is null");
+            if (class_name == String.Empty) throw new System.Exception("class_name is empty");
+
+            m_wnd_proc_delegate = CustomWndProc;
+
+            // Create WNDCLASS
+            WNDCLASS wind_class = new WNDCLASS();
+            wind_class.lpszClassName = class_name;
+            wind_class.lpfnWndProc = System.Runtime.InteropServices.Marshal.GetFunctionPointerForDelegate(m_wnd_proc_delegate);
+
+            UInt16 class_atom = RegisterClassW(ref wind_class);
+
+            int last_error = System.Runtime.InteropServices.Marshal.GetLastWin32Error();
+
+            if (class_atom == 0 && last_error != ERROR_CLASS_ALREADY_EXISTS)
+            {
+                throw new System.Exception("Could not register window class");
+            }
+
+            // Create window
+            m_hwnd = CreateWindowExW(
+                0,
+                class_name,
+                String.Empty,
+                0,
+                0,
+                0,
+                0,
+                0,
+                IntPtr.Zero,
+                IntPtr.Zero,
+                IntPtr.Zero,
+                IntPtr.Zero
+            );
+        }
+
+        private static IntPtr CustomWndProc(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam)
+        {          
+            switch (msg)
+            {
+                case WM_COPYDATA:
+                    CopyDataStruct message = (CopyDataStruct)Marshal.PtrToStructure(lParam, typeof(CopyDataStruct));
+                    update(message.lpData);
+                    break;
+                default:
+                    break;
+            }
+            return DefWindowProcW(hWnd, msg, wParam, lParam);
+        }
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)] 
+        public struct CopyDataStruct
+        {
+            public IntPtr dwData;
+            public int cbData;
+            [MarshalAs(UnmanagedType.LPWStr)]
+            public string lpData;
+        }
+        const uint WM_COPYDATA = 0x004A;
+        static Action<string> update;
+        private WndProc m_wnd_proc_delegate;
+    }
+}
